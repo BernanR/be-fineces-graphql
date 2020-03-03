b127605e11be077203263468bd17b3160afee32b
Jessica <alves.bernan@gmail.com>
2020-03-02 22:21:08 -0300

    Query para trazer o total de Records

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,4 @@
+const moment = require('moment')
 /*
   obs: Todas as querys utilizadas aqui, devem ser referenciadas primeiramente no arquivo
   schema.graphql, nesse arquivo serão expostos somente as querys que serão utilizadas no projeto,
@@ -67,9 +68,84 @@ function categories(_, { operation }, ctx, info){
   },info)
 }
 
+function records(_, { month, type, accountsIds, categoriesIds }, ctx, info){
+  const userId = getUserId(ctx)
+
+  let AND = [ { user: { id: userId } } ]
+  AND = !type ? AND : [ ...AND , { type } ]
+
+  AND = !accountsIds || accountsIds.length === 0
+  ? AND
+  : [ 
+    ...AND,
+    { OR: accountsIds.map( id => ({ account: { id } })) }
+  ]
+
+  AND = !categoriesIds || categoriesIds.length === 0
+  ? AND
+  : [ 
+    ...AND,
+    { OR: categoriesIds.map(id => ({ category: { id } })) }
+  ]
+
+  if(month) {
+    const date = moment(month, 'MM-YYYY') //06-2019
+    const startDate = date.startOf('month').toISOString()
+    const endDate = date.endOf('month').toISOString()
+    AND = [
+      ...AND,
+      {date_gte: startDate},
+      {date_lte: endDate},
+    ]
+  }
+
+  return ctx.db.query.records({
+    where: { AND },
+    orderBy: 'date_ASC'
+  }, info )
+
+}
+
+function totalBalance(_, {date}, ctx, info){
+  const userId = getUserId(ctx)
+  const dateISO = moment(date, 'YYYY-MM-DD').endOf('day').toISOString()
+  const pgSchema =  `${process.env.PRISMA_SERVICE}$${process.env.PRISMA_STAGE}`
+  const mutation = `
+    mutation TotalBalance($database: PrismaDatabase, $query: String!){
+      executeRaw(database: $database, query: $query)
+    }
+  `
+  const variables = {
+    database: 'default',
+    query: `
+      select 
+      sum(rec.amount) as totalbalance
+      from "${pgSchema}"."Record" rec
+        inner join "${pgSchema}"."_RecordToUser" use
+          on use."A" = rec."id"        
+        where use."B" = '${userId}'
+        and rec."date" <= '${dateISO}'
+    `
+  }
+
+  /* 
+    não foi possível utilizar o prisma bilding, para trabalhar com querys personalizadas
+    por isso o uso do prisma client, que no caso não há problemas, já que estamos trazendo 
+    somentes alguns campos em espeficifo.
+  */
+
+  return ctx.prisma.$graphql(mutation, variables)
+    .then(response => {
+      const totalBalance = response.executeRaw[0].totalbalance;
+      return totalBalance ? totalBalance : 0 ;
+    })
+
+}
 
 module.exports = {
   accounts,
   categories,
-  user
+  user,
+  records,
+  totalBalance
 }
